89217f6bddb4cc2237ab03071cfba5a9aca10490
Aria Gharachorlou <aria.gharachorlou@kcl.ac.uk>
2026-03-06 19:31:40 +0000

    main(): Create initial code for main and add imports to requirement.txt

---
+++ TPCRP_Algorithm/main.py
@@ -0,0 +1,16 @@
+import os 
+import numpy as np 
+import torch
+
+
+def run_pipelin():
+    encoder = None 
+
+    os.makedirs("results", exist_ok=True)
+    np.save("results/......") # adjust 
+    print(f"Saved ::: Name ") # adjust 
+    return None 
+
+
+if __name__ == '__main__':
+    pass # Run pipeline()
