36090c4fbbef0e69c99d9abdac1d200ca34735ea
Aria Gharachorlou <aria.gharachorlou@kcl.ac.uk>
2026-03-16 23:15:54 +0000

    main(): Create general CIFAR10 structure for a data loader in supervised_training.py

--- Evaluation_Metrics/supervised_training.py
+++ Evaluation_Metrics/supervised_training.py
@@ -10,11 +10,46 @@ from torch.utils.data import Dataset, DataLoader, Subset
 
 
 class CIFAR10Subset(Dataset):
-    pass 
+    
+
+    def __init__(self, base_dataset, indices):
+        self.base = base_dataset 
+        self.indices = indices 
+
+
+    def __len__(self):
+        return len(self.indices)
+    
+    def __getitem__(self, idx):
+        real_idx = self.indices[idx]
+        return self.base[real_idx]
 
 
 def get_CIFAR10Loaders(selected_indices, batch_size=128):
-    pass 
+    transforms_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorrizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            (0.4914, 0.4822, 0.4465),
+            (0.2470, 0.2435, 0.2616)
+        ),
+    ])
+
+    transform_test = transforms.Compoes([
+        transforms.ToTensor(),
+        transforms.Normalize(
+            (0.4914, 0.4822, 0.4465),
+            (0.2470, 0.2435, 0.2616)
+        ),
+    ])
+
+    train_base = torchvision.datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transforms_train
+    )
+    test_set = torch.vision.datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transform_test
+    )
 
 
 def build_resnet18():
